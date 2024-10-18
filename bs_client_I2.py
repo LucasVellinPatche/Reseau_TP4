1a22d797ff2916ba5ff86823a305f78dafe72d61
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2024-10-18 15:51:26 +0200

    je sais pas utiliser encode T-T

--- bs_client_I2.py
+++ bs_client_I2.py
@@ -7,11 +7,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.connect((host, port))
     print(f"Connecté avec succès au serveur {host} sur le port {port}")
+    msg = input("Que veux-tu envoyer au serveur : ")
+    msg.encode
+    s.sendall(msg)
+    data = s.recv(1024)
 except:
     print("Ça marche pas chef, ça se co pas, déso.")
-msg = input("Que veux-tu envoyer au serveur : ").encode
-s.sendall(msg)
-data = s.recv(1024)
 s.close()
 print(f"Le serveur a répondu {repr(data)}")
 exit(0)
