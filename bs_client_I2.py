43681abcaf3059aa04d7eacd523586a134738044
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2024-10-18 16:13:54 +0200

    parenthese diff?

--- bs_client_I2.py
+++ bs_client_I2.py
@@ -7,11 +7,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     s.connect((host, port))
     print(f"Connecté avec succès au serveur {host} sur le port {port}")
-    s.sendall(input("Que veux-tu envoyer au serveur : "))
+    s.sendall(input("Que veux-tu envoyer au serveur : ").encode())
     data = s.recv(1024)
     s.close()
     print(f"Le serveur a répondu {repr(data)}")
 except Exception as e:
     print("On dirait qu'il y a eu un soucis, déso.")
-    print(f"L'erreur native est : {e.message}")
+    print(f"L'erreur native est : {e}")
 exit(0)
