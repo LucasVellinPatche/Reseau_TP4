0a8e08aee2f68d178bdc7969e0191cd299ea508f
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2024-10-31 12:17:46 +0100

    TP4 II2A, logs v1

--- bs_server_II2A.py
+++ bs_server_II2A.py
@@ -5,10 +5,28 @@ import argparse
 from os import path, mkdir
 import logging
 
-LOG_FILE = "/var/log/server_tp4/server.log"
+LOG_FILE = "/var/log/bs_server/bs_server.log"
 
-if path.isdir("/var/log/server_tp4") != True:
-    mkdir("/var/log/server_tp4")
+class CustomFormatter(logging.Formatter):
+    white = "\e[97m"
+    yellow = "\e[93m"
+    nc = "\e[0m"
+    time = "%(asctime)s"
+    level = "%(levelname)s"
+    msg = "%(message)s"
+
+    FORMATS = {
+        logging.INFO: time + white + level + nc + msg,
+        logging.WARNING: time + yellow + level + nc + msg,
+    }
+
+def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+if path.isdir("/var/log/bs_server") != True:
+    mkdir("/var/log/bs_server")
 
 if path.isfile(LOG_FILE) != True:
     log = open(LOG_FILE, "x")
@@ -16,6 +34,7 @@ if path.isfile(LOG_FILE) != True:
 logging.basicConfig(level=logging.INFO, datefmt="%Y-%m-%d %H:%M", format="%(asctime)s %(levelname)s %(message)s")
 logger = logging.getLogger(__name__)
 console_handler = logging.StreamHandler()
+console_handler.setFormatter(CustomFormatter)
 file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8", filemode="a")
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
@@ -54,21 +73,33 @@ if trouve != 1:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))  
 s.listen(1)
+logger.info(f"Le serveur tourne sur {host}:{port}")
+
+try:
+    s.timeout(1)
+except:
+    logger.warning("Aucun client depuis plus de une minute.")
+
 conn, addr = s.accept()
+logger.info(f"Un client {addr[0]} s'est connecté.")
+
 
 while True:
 
     try:
         data = conn.recv(1024)
+        data = str(data)
         if not data: break
-        print(f"Un client vient de se co et son IP c'est {addr[0]}.")
-        print(f"Données reçues du client : {data}")
-        if search(".*meo.*", str(data)):
+        logger.info(f"Le client {addr[0]} a envoyé {data}.")
+        if search(".*meo.*", data):
             conn.sendall(b"Meo a toi confrere.")
-        elif search(".*waf.*", str(data)):
+            logger.info(f"Réponse envoyée au client {addr[0]} : Meo a toi confrere.")
+        elif search(".*waf.*", data):
             conn.sendall(b"ptdr t ki")
+            logger.info(f"Réponse envoyée au client {addr[0]} : ptdr t ki")
         else:
             conn.sendall(b"Mes respects humble humain.")
+            logger.info(f"Réponse envoyée au client {addr[0]} : Mes respects humble humain.")
 
     except socket.error:
         print("Error Occured.")
