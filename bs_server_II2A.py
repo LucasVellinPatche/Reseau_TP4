499f67bf90125e4659629276c7a1bec6ba54b383
LucasVellinPatche <124413478+Lsmile974@users.noreply.github.com>
2024-11-21 16:34:52 +0100

    8e test d'affichage

--- bs_server_II2A.py
+++ bs_server_II2A.py
@@ -83,8 +83,8 @@ while True:
     try:
         data = conn.recv(1024)
         data = data.decode()
-        if not data: break
         print(type(data))
+        if not data: break
         logger.info(f"Le client {addr[0]} a envoyé {data}.")
         if search(".*meo.*", data):
             conn.sendall(b"Meo a toi confrere.")
